a5998bfa4ee2e1d8fb1248ff53a083b5f2f91b72
Jeevant <jeevantmudgil10@gmail.com>
2026-07-30 19:52:41 +0530

    feat: add JWT utility functions for token management and user authentication

---
+++ Backend/utils/helpers.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+
+function getToken(phone, user) {
+  return jwt.sign(
+    {
+      id: user._id,
+      phone: phone,
+      role: "user"
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+}
+
+const createAccessToken  = payload =>
+
+  jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { 
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN 
+  });
+
+  const createRefreshToken = payload =>
+  jwt.sign(
+    payload, 
+    process.env.REFRESH_TOKEN_SECRET, { 
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN 
+    });
+
+    const verifyToken = (req, res, next) => {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) return res.status(401).json({ message: "No token provided" });
+
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) return res.status(403).json({ message: "Invalid token" });
+
+          req.user = decoded; // contains user.id and role (admin/user)
+          next();
+        });
+      };
+
+      const isAdmin = (req, res, next) => {
+        if (!req.user || req.user.role !== "admin") {
+          return res.status(403).json({ message: "Forbidden" });
+        }
+        next();
+      };
+
+      const validateBody = (req, res, next) => {
+        next();
+      };
+
+
+
+
+  module.exports = { createAccessToken, createRefreshToken , getToken , verifyToken , isAdmin , validateBody };
