e47e14ae8451880273852e56d6eb9ed1898a4abd
Jeevant <jeevantmudgil10@gmail.com>
2026-07-30 19:54:41 +0530

    feat: wrap App component with CookiesProvider and AuthProvider for context management

--- Ecommerce_Platform/src/main.jsx
+++ Ecommerce_Platform/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CookiesProvider } from 'react-cookie'
+import { AuthProvider } from '../src/utils/authContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CookiesProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </CookiesProvider>
   </StrictMode>,
 )
